2bd4db65206ba8a24912bf2028d58c5c2e01949a
root <mike.michel@mmbash.de>
2017-01-26 14:55:40 +0000

    cli

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -vvv -H "Content-Type: application/json" -H "Authorization: Bearer $SLOPPYIO_TOKEN" -X PATCH -d '{"image": "'{{ .CommitID }}':'{{ .CommitID }}'"}'  https://api.sloppy.io/v1/apps/hackathon/services/frontend/apps/node
+#curl -vvv -H "Content-Type: application/json" -H "Authorization: Bearer $SLOPPYIO_TOKEN" -X PATCH -d '{"image": "mikemichel:'{{ .CommitID }}'"}'  https://api.sloppy.io/v1/apps/hackathon/services/frontend/apps/node
+
+sloppy show
