d5e61a6557b2ab024d70eaeae16de95ded5f8ed2
Mike <mike.michel@mmbash.de>
2017-01-26 17:17:40 +0100

    use cli

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-curl -vvv -H "Content-Type: application/json" -H "Authorization: Bearer $SLOPPYIO_TOKEN" -X PATCH -d '{"image": "'{{ .CommitID }}':'{{ .CommitID }}'"}'  https://api.sloppy.io/v1/apps/hackathon/services/frontend/apps/node
+set -e
+
+SLOPPY_APITOKEN=${SLOPPY_APITOKEN}
+
+# install latest sloppy.io cli
+curl -L https://files.sloppy.io/sloppy-`uname -s`-`uname -m` > /usr/local/bin/sloppy
+chmod +x /usr/local/bin/sloppy
+
+# deploy new image
+sloppy change -img mikemichel/hackathon-starter:$CI_COMMIT_ID
