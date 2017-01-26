92dae0f2fbaafa16e7afc95883d0d3522296e487
root <mike.michel@mmbash.de>
2017-01-26 15:17:20 +0000

    cli dl

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 #curl -vvv -H "Content-Type: application/json" -H "Authorization: Bearer $SLOPPYIO_TOKEN" -X PATCH -d '{"image": "mikemichel:'{{ .CommitID }}'"}'  https://api.sloppy.io/v1/apps/hackathon/services/frontend/apps/node
 
+set -e
+
+SLOPPY_APITOKEN=${SLOPPY_APITOKEN}
 
 curl -L https://files.sloppy.io/sloppy-`uname -s`-`uname -m` > /usr/local/bin/sloppy
 chmod +x /usr/local/bin/sloppy
